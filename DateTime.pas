65dffc6e7322aebee1119e2cbe49d53b42e23f9d
Яков Батуев <yaxa499@gmail.com>
2024-10-04 18:26:52 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var year := ReadInteger('Введите номер года:');
+  Assert(year > 0);
+  Print($'Этот год является високосным: {year.Divs(4) and not year.Divs(100) or year.Divs(400)}');
+end.
